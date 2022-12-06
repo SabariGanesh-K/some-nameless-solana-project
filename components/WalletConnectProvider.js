e2afffe67d94dce7c09a70973ad6b14e6898a8e4
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2022-12-06 23:27:18 +0530

    initial upload

---
+++ components/WalletConnectProvider.js
@@ -0,0 +1,33 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
+import { ConnectionProvider,WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import  {GlowWalletAdapter,PhantomWalletAdapter,SlopeWalletAdapter} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+import React from 'react'
+
+export const WalletConnectProvider = ({children}) => {
+    const network = WalletAdapterNetwork.Devnet;
+
+    const endpoint = useMemo(()=>{
+        if(network==WalletAdapterNetwork.Devnet){
+            return 'https://distinguished-lingering-log.solana-devnet.discover.quiknode.pro/ee241c814e5d7d13f38879236c6f611cd42d3e5b/'
+
+        }
+        return clusterApiUrl(network)
+    },[network])
+    const wallets = useMemo(()=>[new PhantomWalletAdapter()],[network])
+
+    return (
+        <ConnectionProvider>
+            <WalletProvider wallets = {wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+
+        </ConnectionProvider>
+    )
+
+}
+
